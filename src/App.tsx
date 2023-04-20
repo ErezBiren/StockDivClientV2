8d3809ebc307117eea222f7eef4598d7e2e4d517
Erez Birenholz <biran330@gmail.com>
2023-04-20 22:07:50 +0300

    Add Error not found and Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import ErrorNotFound from "./pages/ErrorNotFound";
 import LoginPage from "./pages/LoginPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <LoginPage />
-    </>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/overview" element={<></>} />
+      <Route path="/ticker/:ticker" element={<></>} />
+      <Route path="/yearlyPaymentMatrix" element={<></>} />
+      <Route path="/monthlyDividendsView/:month" element={<></>} />
+      <Route path="/dividendAlerts" element={<></>} />
+      <Route path="/announcements" element={<></>} />
+      <Route path="/portfolio" element={<></>} />
+      <Route path="/screener" element={<></>} />
+      <Route path="*" element={<ErrorNotFound />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/ErrorNotFound.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function ErrorNotFound() {
+  return (
+    <div className="fullscreen bg-[#2196f3] text-white text-center p-8 flex items-center justify-center">
+      <div>
+        <div className="text-9xl mb-6">404</div>
+
+        <div className="text-4xl opacity-40 mb-8">Oops. Nothing here...</div>
+
+        <Link
+          to="/"
+          className="bg-white text-blue-500 font-bold py-4 px-8 rounded-md uppercase tracking-wide hover:bg-blue-500 hover:text-white transition duration-200"
+        >
+          Go Home
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorNotFound;
