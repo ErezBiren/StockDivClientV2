b2e89cdbc541213b86b53cc49175d164ae19bdda
Erez Birenholz <biran330@gmail.com>
2023-04-23 09:36:19 +0300

    add login window popups

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -12,16 +12,17 @@ import {
   showNotification,
   validateEmail,
 } from "../../utils/utils";
-import {
-  useForgotPasswordMutation,
-  usersApiSlice,
-} from "../users/usersApiSlice";
+import { useForgotPasswordMutation } from "../users/usersApiSlice";
+import Modal, { ModalAction } from "../../components/modal";
 
 export default function Login() {
   const [login] = useLoginMutation();
   const [register] = useRegisterMutation();
-
   const [forgotPassword] = useForgotPasswordMutation();
+
+  const [isWhyEmailOpen, setIsWhyEmailOpen] = useState(false);
+  const [isDisclaimerOpen, setIsDisclaimerOpen] = useState(false);
+  const [isPrivacyPolicyOpen, setIsPrivacyPolicyOpen] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -32,7 +33,6 @@ export default function Login() {
   const [isPwd, setIsPwd] = useState(true);
   const [isRePwd, setIsRePwd] = useState(true);
   const [email, setEmail] = useState<string>();
-  const [newUser, setNewUser] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
   const [confirmationCode, setConfirmationCode] = useState("");
   const [codeSent, setCodeSent] = useState(false);
@@ -182,138 +182,201 @@ export default function Login() {
     letMeIn();
   };
 
+  const customModalActions: ModalAction[] = [
+    {
+      id: "1",
+      text: "ok",
+      className:
+        "text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800",
+      callback: () => {
+        setIsWhyEmailOpen(false);
+        setIsPrivacyPolicyOpen(false);
+        setIsDisclaimerOpen(false);
+      },
+    },
+  ];
+
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <img src={LogoImage} alt="logo" style={{ width: "400px" }} />
-            <h1 className="text-xs font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Your portfolio. Your income.
-            </h1>
-            <form
-              className="space-y-4 md:space-y-6"
-              action="#"
-              onSubmit={handleSubmit}
-            >
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
-                <input
-                  ref={emailRef}
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              {isNewUser && (
+    <>
+      <section className="bg-gray-50 ">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <img src={LogoImage} alt="logo" style={{ width: "400px" }} />
+              <h1 className="text-xs font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                Your portfolio. Your income.
+              </h1>
+              <form
+                className="space-y-4 md:space-y-6"
+                action="#"
+                onSubmit={handleSubmit}
+              >
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Your email
+                  </label>
+                  <input
+                    ref={emailRef}
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
                 <div>
                   <label
                     htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Confirm Password
+                    Password
                   </label>
                   <input
-                    ref={rePasswordRef}
                     type="password"
-                    name="confirm"
-                    id="confirm"
-                    placeholder=" Confirm Password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    onChange={(e) => setRePassword(e.target.value)}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
-              )}
-              <div className="flex items-center justify-between">
-                <div className="flex items-center mb-4">
-                  <input
-                    id="new-user"
-                    type="checkbox"
-                    checked={isNewUser}
-                    onChange={() => setIsNewUser((prev) => !prev)}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                    htmlFor="new-user"
-                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                {isNewUser && (
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Confirm Password
+                    </label>
+                    <input
+                      ref={rePasswordRef}
+                      type="password"
+                      name="confirm"
+                      id="confirm"
+                      placeholder=" Confirm Password"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      onChange={(e) => setRePassword(e.target.value)}
+                    />
+                  </div>
+                )}
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center mb-4">
+                    <input
+                      id="new-user"
+                      type="checkbox"
+                      checked={isNewUser}
+                      onChange={() => setIsNewUser((prev) => !prev)}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="new-user"
+                      className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >
+                      New User
+                    </label>
+                  </div>
+                  <a
+                    onClick={handleForgotPassword}
+                    href="#"
+                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
-                    New User
-                  </label>
+                    Forgot password?
+                  </a>
                 </div>
-                <a
-                  onClick={handleForgotPassword}
-                  href="#"
-                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
-              {codeSent && (
-                <div>
-                  <label
-                    htmlFor="confirmationCode"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    A confirmation code was sent to your email
-                  </label>
-                  <input
-                    ref={confirmationCodeRef}
-                    type="text"
-                    name="confirmationCode"
-                    id="confirmationCode"
-                    placeholder=""
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    onChange={(e) => setConfirmationCode(e.target.value)}
-                  />
-                </div>
-              )}
+                {codeSent && (
+                  <div>
+                    <label
+                      htmlFor="confirmationCode"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      A confirmation code was sent to your email
+                    </label>
+                    <input
+                      ref={confirmationCodeRef}
+                      type="text"
+                      name="confirmationCode"
+                      id="confirmationCode"
+                      placeholder=""
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      onChange={(e) => setConfirmationCode(e.target.value)}
+                    />
+                  </div>
+                )}
 
-              <button
-                disabled={disableLetMeIn}
-                type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                LET ME IN
-              </button>
-              <div className="flex flex-row justify-between">
-                <a href="#" className="text-sm font-medium">
-                  Privacy policy
-                </a>
-                <a href="#" className="text-sm font-medium ">
-                  Disclaimer
-                </a>
-                <a href="#" className="text-sm font-medium ">
-                  Why email?
-                </a>
-              </div>
-            </form>
+                <button
+                  disabled={disableLetMeIn}
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  LET ME IN
+                </button>
+                <div
+                  className="flex flex-row justify-between"
+                  onClick={() => setIsPrivacyPolicyOpen(true)}
+                >
+                  <a href="#" className="text-sm font-medium">
+                    Privacy policy
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm font-medium "
+                    onClick={() => setIsDisclaimerOpen(true)}
+                  >
+                    Disclaimer
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm font-medium "
+                    onClick={() => setIsWhyEmailOpen(true)}
+                  >
+                    Why email?
+                  </a>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Modal
+        title="StockDiv"
+        actions={customModalActions}
+        isOpen={isPrivacyPolicyOpen}
+        onClose={() => setIsPrivacyPolicyOpen(false)}
+      >
+        Privacy Policy: The application collects only the following user's data:
+        email, password (encrypted) and transactions list as entered by the
+        user. This data won't be sold to any 3rd party. The data is stored in
+        Google Firebase.
+      </Modal>
+      <Modal
+        title="StockDiv"
+        actions={customModalActions}
+        isOpen={isDisclaimerOpen}
+        onClose={() => setIsDisclaimerOpen(false)}
+      >
+        Disclaimer: StockDiv and the information contained herein is not
+        intended to be a source of advice or credit Analysis with respect to the
+        material presented, and the information and/or documents contained in
+        StockDiv do not constitute an investment advice. The information
+        contained in StockDiv is for general information purposes only. You
+        should not rely upon the information on StockDiv as a basis for making
+        any business, legal or any other decisions.
+      </Modal>
+      <Modal
+        title="StockDiv"
+        actions={customModalActions}
+        isOpen={isWhyEmailOpen}
+        onClose={() => setIsWhyEmailOpen(false)}
+      >
+        Your email will only be used for support, account deletion and password
+        change. Your email won't be sold nor given to any third party
+        whatsoever. A confirmation code will be sent to make sure the email is
+        yours.
+      </Modal>
+    </>
   );
 }
