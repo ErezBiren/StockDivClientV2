de0e631e55c8d798fc5122adc650c6ec0c3d423a
Erez Birenholz <biran330@gmail.com>
2023-05-02 23:26:46 +0300

    Add ticker api functions

--- src/features/ticker/tickerApiSlice.ts
+++ src/features/ticker/tickerApiSlice.ts
@@ -1,5 +1,10 @@
 import { apiSlice } from "../../app/api/apiSlice.ts";
 
+const getTickerQuery = (ticker: string, endpoint: string) => ({
+  url: `ticker/${ticker}/${endpoint}`,
+  method: "GET",
+});
+
 export const tickerApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getTickerSearch: builder.query({
@@ -9,13 +14,32 @@ export const tickerApiSlice = apiSlice.injectEndpoints({
       }),
     }),
     getTickerNews: builder.query({
-      query: (ticker: string) => ({
-        url: `ticker/${ticker}/news`,
-        method: "GET",
-      }),
+      query: (ticker: string) => getTickerQuery(ticker, "news"),
+    }),
+    getTickerName: builder.query({
+      query: (ticker: string) => getTickerQuery(ticker, "name"),
+    }),
+    getTickerLogo: builder.query({
+      query: (ticker: string) => getTickerQuery(ticker, "logo"),
+    }),
+    getTickerCurrency: builder.query({
+      query: (ticker: string) => getTickerQuery(ticker, "currency"),
+    }),
+    getTickerFrequency: builder.query({
+      query: (ticker: string) => getTickerQuery(ticker, "frequency"),
+    }),
+    getTickerDailyChange: builder.query({
+      query: (ticker: string) => getTickerQuery(ticker, "dailyChange"),
     }),
   }),
 });
 
-export const { useLazyGetTickerSearchQuery, useGetTickerNewsQuery } =
-  tickerApiSlice;
+export const {
+  useLazyGetTickerSearchQuery,
+  useGetTickerNewsQuery,
+  useGetTickerNameQuery,
+  useGetTickerLogoQuery,
+  useGetTickerCurrencyQuery,
+  useGetTickerFrequencyQuery,
+  useGetTickerDailyChangeQuery
+} = tickerApiSlice;
