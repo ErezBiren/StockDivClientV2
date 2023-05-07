7d9b53277ea92cc310c8b7a37abd2b4155b5984b
Erez Birenholz <biran330@gmail.com>
2023-05-07 19:06:33 +0300

    remove redundent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: {
-          50: "#eff6ff",
-          100: "#dbeafe",
-          200: "#bfdbfe",
-          300: "#93c5fd",
-          400: "#60a5fa",
-          500: "#3b82f6",
-          600: "#2563eb",
-          700: "#1d4ed8",
-          800: "#1e40af",
-          900: "#1e3a8a",
-        },
+       
         cardBackground:"#E1F5FE",
         headerBackground: "#c8e6c9",
         iconsColor:"#2196f3"
